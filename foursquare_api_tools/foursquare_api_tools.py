f6e028da1799252f54e7a67c8c17693a294029d8
Diego Carrasco G <557703+dacog@users.noreply.github.com>
2019-02-27 11:26:07 +0100

    added git ignore and venues_explore_near function
    cloned venues_explore function and updated accoriding to foursquare api docs for "near" argument.

--- foursquare_api_tools/foursquare_api_tools.py
+++ foursquare_api_tools/foursquare_api_tools.py
@@ -58,6 +58,63 @@ def venues_explore(client,lat,lng, limit=100, verbose=0, sort='popular', radius=
         print('ERROR: offset value per Foursquare API is up to 50. Please use a lower value.')
     return df_a.reset_index()
 
+def venues_explore_near(client,near, limit=100, verbose=0, sort='popular', radius=100000, offset=1, day='any',query=''):
+    '''funtion to get n-places using explore in foursquare, where n is the limit when calling the function.
+    This returns a pandas dataframe with name, city ,country, near, address and main category as columns.
+    "near" argument searches within the bounds of the geocode for a string naming a place in the world.
+    Arguments: *client, *near, limit (defaults to 100), radius (defaults to 100000, max according to api docs), verbose (defaults to 0), offset (defaults to 1), day (defaults to any)'''
+    # create a dataframe
+    df_a = pd.DataFrame(columns=['Name', 
+    'City', 
+    'Latitude',
+    'Longitude',
+    'Category',
+    'Address'])
+    if offset<=50:
+        for i_offset in range(0,offset):
+            #get venues using client https://github.com/mLewisLogic/foursquare
+            venues = client.venues.explore(params={'near':near,
+            'limit':limit, 
+            'intent' : 'browse',
+            'sort':sort, 
+            'radius':radius, 
+            'offset':i_offset,
+            'day':day,
+            'query':query
+            })
+            venues=venues['groups'][0]['items']
+            df_venues = pd.DataFrame.from_dict(venues)
+            df_venues['venue'][0]
+            #print('limit', limit, 'sort', sort, 'radius', radius)
+            for i, value in df_venues['venue'].items():
+                if verbose==1:
+                    print('i', i, 'name', value['name'])
+                venueName=value['name']
+                try:
+                    venueCity=value['location']['city']
+                except:
+                    venueCity=''
+                venueCountry=value['location']['country']
+                venueLat=value['location']['lat']
+                venueLng=value['location']['lng']
+                venueCountry=value['location']['country']
+                try:
+                    venueAddress=value['location']['address']
+                except:
+                    venueAddress=''
+                venueCategory=value['categories'][0]['name']
+                df_a=df_a.append([{'Name':venueName, 
+                                   'City':venueCity,
+                                   'Country':venueCountry,
+                                   'Latitude':venueLat,
+                                   'Longitude':venueLng,
+                                   'Category':venueCategory,
+                                   'Address':venueAddress
+                                  }])
+    else:
+        print('ERROR: offset value according to Foursquare API is up to 50. Please use a lower value.')
+    return df_a.reset_index()
+
 def get_categories():
     '''Function to get a Pandas DataFrame of all categories in Foursquare as listed in https://developer.foursquare.com/docs/resources/categories
     It uses json_normalize to get nested information and return a DataFrame with main, sub and sub-sub categories name and ID'''
